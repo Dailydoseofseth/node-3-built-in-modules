841ec27ac8a8b9efd8bd8b919d51b5954506f83c
dailydoseofseth <dailydoseofseth@gmail.com>
2026-06-25 14:24:14 -0600

    ADD birthstone finder

--- birthstone-finder/warmup-birthstone-finder.js
+++ birthstone-finder/warmup-birthstone-finder.js
@@ -21,3 +21,14 @@ const birthstones = {
   November: "Topaz or Citrine",
   December: "Tanzanite, Zircon, or Turquoise",
 };
+
+// Get the month from CLI ARGuments
+// process.argv is an array that contains the command line arguments passed to the Node.js process
+const month = process.argv[2];
+
+// this is why month is in ARRAY brackets, bc we are using the value of the variable month as the KEY to access the VALUE in the birthstones object
+if (birthstones[month]) {
+  console.log(`The birthstone for ${month} is ${birthstones[month]}.`);
+} else {
+  console.log("Invalid month. Please enter a valid month.");
+}
