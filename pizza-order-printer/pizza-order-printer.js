494d9d3d104a61dca859bcc660eab7bad9626a1a
dailydoseofseth <dailydoseofseth@gmail.com>
2026-06-29 15:12:30 -0600

    Fix pizza-order-printer: call getPizzaOrder and correct data.json path

--- pizza-order-printer/pizza-order-printer.js
+++ pizza-order-printer/pizza-order-printer.js
@@ -5,3 +5,22 @@ Make a Node.js app that prints pizza orders to the console.
 
 Check the README.md file for instructions.
 ------------------------- */
+
+// import .fs module to read JSON file
+import fs from "fs/promises";
+
+// Get user inputted pizza order
+const order = process.argv[2];
+
+// Find the pizza order and print it to the console
+async function getPizzaOrder() {
+  // read pizza order data from JSON file
+  const data = await fs.readFile("./pizza-order-printer/data.json", "utf-8");
+  const pizzaOrders = JSON.parse(data);
+  // check to see if awaiting the data from the JSON file worked
+  console.log("DATA:", data);
+  // now check to see if the pizzaOrders object was created correctly
+  console.log("pizzaOrders:", pizzaOrders);
+}
+
+getPizzaOrder();
